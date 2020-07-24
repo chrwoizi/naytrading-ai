3b69ae842d12eec0da3de72706c79b9ced696443
chrwoizi <christian@woizischke.de>
2020-07-25 01:30:00 +0200

    use specific tensorflow version

--- train_buying.bat
+++ train_buying.bat
@@ -1,6 +1,6 @@
-pip install numpy
-pip install tensorflow
-pip install tensorflow-gpu
-REM pip install matplotlib
-python src\train.py --action=buy --model_dir=buying --train_file=buying_train.csv --test_file=buying_test.csv --repeat_train_data=1 --epochs=134
+python -m pip install numpy
+python -m pip install tensorflow==1.15
+python -m pip install tensorflow-gpu==1.15
+REM python -m pip install matplotlib
+python src\train.py --action=buy --model_dir=buying --train_file=buying_train_norm.csv --test_file=buying_test_norm.csv --repeat_train_data=1 --epochs=134
 pause
